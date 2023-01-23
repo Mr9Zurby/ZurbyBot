4bbf8176ef1402afa31396179ff8f9891a3d3cf6
QuentinMtx <quentin.montoux@gmail.com>
2023-01-23 19:28:08 +0100

    Ajout blocage spam des rôles

--- commandes/spam.js
+++ commandes/spam.js
@@ -8,10 +8,11 @@ module.exports = {
 		console.log(`2 1er car: ${pseudo.substring(0, 2)}`)
 		if (pseudo.charAt() == '@') await message.reply(`Cette personne n'est pas présente sur ce discord.`);
 		else if (pseudo.charAt() != '@' && pseudo.substring(0, 2) != '<@') await message.reply(`Tu as oublié le @ devant le pseudo`);
+		else if (pseudo.substring(0, 3) == '<@&') await message.reply(`Spam pas un rôle idiot..`);
 		else if (!parseInt(nombre)) await message.reply(`Met un chiffre idiot..`);
 		else if (parseInt(nombre) > 15) await message.reply(`Calme toi sur le nombre de spam BG.`);
 
-		if (pseudo.substring(0, 2) == '<@' && nombre <= 15) {
+		if (pseudo.substring(0, 2) == '<@' && pseudo.substring(0, 3) != '<@&' && nombre <= 15) {
 			for (let pas = 0; pas < nombre; pas++) {
 				await message.reply(`Hey ${pseudo} t'es attendu !`);
 				await new Promise(res => setTimeout(res, 1000));
